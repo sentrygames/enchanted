64d099d5dc721a7bcbcdc411d37be8cddcd6ceb6
sentrygames <sivsome@gamergamestudios.xyz>
2024-08-28 14:22:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://www.nvidia.com';
+const nggUrl = 'https://slither.io/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'www.nvidia.com') {
+    if (req.headers.host === 'slither.io) {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
