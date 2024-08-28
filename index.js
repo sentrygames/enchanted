33a73edfe78db641c66a9f2569bce05fc67022de
sentrygames <sivsome@gamergamestudios.xyz>
2024-08-28 14:12:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://www.xbox.com';
+const nggUrl = 'https://www.nvidia.com';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'www.xbox.com') {
+    if (req.headers.host === 'www.nvidia.com') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
