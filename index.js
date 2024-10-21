3dc9b88271b42d4fed5425c7cb960a92e09b46f5
sentrygames <sivsome@gamergamestudios.xyz>
2024-10-21 13:46:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://shellshock.io';
+const nggUrl = 'https://sivsome.framer.website';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'shellshock.io) {
+    if (req.headers.host === 'sivsome.framer.website) {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
